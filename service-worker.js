1820893fea8de08e4bc780c717789048c98585b1
Tracey <positivity864@gmail.com>
2026-08-24 09:02:14 +1000

    Refresh PWA cache for luxury Genevieve theme

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='genevieve-money-v2-1';
-const ASSETS=['/','/index.html','/styles.css','/app.js','/legacy-app.js','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
+const CACHE='genevieve-money-v3-luxury';
+const ASSETS=['/','/index.html','/styles.css','/luxury.css','/app.js','/legacy-app.js','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
